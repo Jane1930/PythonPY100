ea904f31ee176fc1ddc15d98ccdde582a391688c
Jane1930 <92628447+Jane1930@users.noreply.github.com>
2021-11-03 23:19:15 +0300

    Delete Занятие3 directory

--- Занятие3/Лабораторные_задания/task2_3/main.py
+++
@@ -1,6 +0,0 @@
-if __name__ == "__main__":
-    def split_join_str():
-        str_ = input().split()
-        return " ".join(str_)
-
-print(split_join_str())
--- Занятие3/Практические_задания/task1_5/main.py
+++
@@ -1,9 +0,0 @@
-def counter():
-    count_ = count + 1
-
-    return count_
-
-
-if __name__ == "__main__":
-    count = 0
-    print(counter())  # что будет выведено?
